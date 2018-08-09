b4ba5b3edbdaf3d459f1a406ab1d30d1565a25a5
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2018-08-09 11:27:15 +0300

    lombok - 1

--- src/main/java/etu/spb/spring/demo/Person.java
+++ src/main/java/etu/spb/spring/demo/Person.java
@@ -1,5 +1,43 @@
 package etu.spb.spring.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public final class Person {
 
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return getAge() == person.getAge() &&
+                Objects.equals(getFirstName(), person.getFirstName()) &&
+                Objects.equals(getLastName(), person.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAge(), getFirstName(), getLastName());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
---
+++ src/test/java/etu/spb/spring/demo/PersonTest.java
@@ -0,0 +1,24 @@
+package etu.spb.spring.demo;// import etu.spb.spring.demo.Person
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonTest {
+    @Test
+    @DisplayName("\"SetAge\" method works correctly")
+    void testSetAge() {
+        // given
+        Person person = new Person();
+        Person person2 = new Person(21, "Вася", "Петичкин");
+
+        // when
+        person.setAge(55);
+
+        // then
+        assertEquals(person.getAge(), 55);
+        assertEquals(person2.getFirstName(), "Вася");
+    }
+
+}
